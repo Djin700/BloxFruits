c1365e244b977c770b971fedc75c556378151b91
Djin700 <alekseidjin700@gmail.com>
2025-01-02 05:36:47 +0900

    Update Fruits.lua

--- Fruits.lua
+++ Fruits.lua
@@ -4,29 +4,29 @@ local blockname = {"Banana","Pineapple","Apple"}
 local FarmSpeed = 1
 local ServerHop = true
 
---//Hop//--
-local Http = game:GetService("HttpService")
-local TPS = game:GetService("TeleportService")
-local Api = "https://games.roblox.com/v1/games/"
-local _place,_id = game.PlaceId, game.JobId
-local _servers = Api.._place.."/servers/Public?sortOrder=Asc&limit=12"
-
-local function ServerList(cursor)
-	local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
-	return Http:JSONDecode(Raw)
-end
-
-local function pcallhop()
-	local success, message = pcall(function()
-		local Servers = ServerList()
-		local Server = Servers.data[math.random(1,#Servers.data)]
-		TPS:TeleportToPlaceInstance(_place, Server.id, plr)
-	end)
-end
-
+--//ServerHop//--
 local function ServerHop()
-	while task.wait(3) do
-		pcallhop()
+	local Player = game.Players.LocalPlayer    
+	local Http = game:GetService("HttpService")
+	local TPS = game:GetService("TeleportService")
+	local Api = "https://games.roblox.com/v1/games/"
+
+	local _place,_id = game.PlaceId, game.JobId
+	-- Asc for lowest player count, Desc for highest player count
+	local _servers = Api.._place.."/servers/Public?sortOrder=Asc&limit=10"
+	
+	local function ListServers(cursor)
+		local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
+		return Http:JSONDecode(Raw)
+	end
+
+	-- choose a random server and join every 2 minutes
+	while wait(1) do
+		--freeze player before teleporting to prevent synapse crash?
+		Player.Character.HumanoidRootPart.Anchored = true
+		local Servers = ListServers()
+		local Server = Servers.data[math.random(1,#Servers.data)]
+		TPS:TeleportToPlaceInstance(_place, Server.id, Player)
 	end
 end
 --//❤//--
